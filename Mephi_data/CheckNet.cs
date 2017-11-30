c3900d74fe83eb5ce576378861f7f55440946f30
Andrew Gilenko <keshe4k@yandex.ru>
2017-11-30 23:28:11 +0300

    first commit

---
+++ Mephi_data/CheckNet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.IO;
+
+namespace Mephi_data
+{
+    public class CheckNet
+    {
+        #region StatusCode
+        private static IEnumerable<HttpStatusCode> onlineStatusCodes = new[]
+        {
+        HttpStatusCode.Accepted,
+        HttpStatusCode.Found,
+        HttpStatusCode.OK,
+    };
+        #endregion
+        public static bool isOnline(string URL)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Timeout = 3000;
+            try
+            {
+                WebResponse resp = request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                if (((HttpWebResponse)e.Response).StatusCode == HttpStatusCode.NotFound)
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool CheckURL(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = 15000;
+            request.Method = "HEAD";
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+
+    }
+}
--- Mephi_data/Program.cs
+++ Mephi_data/Program.cs
@@ -13,17 +13,33 @@ namespace Mephi_data
     {
         static void Main(string[] args)
         {
-            string URL = "https://home.mephi.ru/study_groups/2783/schedule";
+            string URL = "https://home.mephi.ru/study_groups/2873/schedule";
             string path = @"D:\text.txt";
             string data = @"D:\data.txt";
             string html = "";
+
+
+            if (CheckNet.CheckURL(URL))
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                Console.Read();
+                return;
+            }
+ 
 
             HttpWebRequest proxy_request = (HttpWebRequest)WebRequest.Create(URL);
             proxy_request.Method = "GET";
             proxy_request.ContentType = "application/x-www-form-urlencoded";
             proxy_request.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.89 Safari/532.5";
             proxy_request.KeepAlive = true;
+
+
             HttpWebResponse resp = proxy_request.GetResponse() as HttpWebResponse;
+
             using (StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.GetEncoding("utf-8"))) html = sr.ReadToEnd();
             html = html.Trim();
             File.WriteAllText(path, html + Environment.NewLine);
@@ -57,7 +73,7 @@ namespace Mephi_data
         {
             for (int i=0;i<html.Length;i++)
             {
-                if ((html[i] == '<')||(html[i] == '>')|| (html[i] == '/'))
+                if ((html[i] == '<')||(html[i] == '>')||(html[i] == '/'))
                 {
                     int index1 = html.IndexOf(">");
                     int index2 = html.IndexOf("</");
